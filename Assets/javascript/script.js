67fc62ef717bea4f3b1d2e86d1da2e6642ba48de
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-07-03 12:33:57 +0930

    Added a psuedo code for javascript functions

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,11 +1,43 @@
+
 let city = "",
     url = "",
     APIKey="",
     currentForeCastUrl = "",
     fiveDayForeCastUrl = "",
     cities = "",
-    citiesDiv = document.getElementById("searched_cities_container");
+    previousCitiesDiv = document.getElementById("prevously-searched-citiesr");
+    
+    let citiesArray = [];
     init();
     listClicker();
     searchClicker();
 
+// Retreive previously saved search cities
+function init() {
+    
+    renderButtons();
+}
+// Save searched cities to local storage
+function storeCities() {
+
+}
+
+// Create buttons for each searched cities from array
+function renderButton() {
+
+}
+
+// On click event for previously searched city button(s)
+function listClicker() {
+
+}
+
+// On Click event for search button
+function searchClicker() {
+
+}
+
+// Run 2 API calls, one for current forecast and one for five day forecast
+function APIcalls() {
+    
+}
