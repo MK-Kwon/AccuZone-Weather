274732ef6e52136499fac4326bc8bb66b6506f75
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-07-03 19:38:21 +0930

    Added function to create buttons with searched cities

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -13,6 +13,7 @@ let city = "",
 
 // Retreive previously saved search cities then create buttons of the data
 function init() {
+
     let savedCities = JSON.parse(localStorage.getItem("citiesArray"));
     if(savedCities === null) {
         return;
@@ -21,12 +22,27 @@ function init() {
 }
 // Save searched cities to an array in local storage
 function storeCities() {
+
     localStorage.setItem("citiesArray", JSON.stringify(citiesArray));
 }
 
 // Create buttons for each searched cities from array
 function renderButton() {
-    
+
+    let citiesDiv = document.getElementById(prevously-searched-cities);
+
+    if(citiesArray === null) {
+        return;
+    }
+    let uniqueCities = [...new Set(citiesArray)];
+    for (let i = 0; i < uniqueCities.length; i++) {
+
+        let buttonEl = document.createElement("button");
+        buttonEl.textContent = uniqueCities[i];
+        buttonEl.setAttribute("class", "btnSearch btn-outline-secondary");
+        citiesDiv.appendChild(buttonEl);
+        listClicker();
+    }
 }
 
 // On click event for previously searched city button(s)
